6edf7be24f249eb9c9aca6e416bcb74a32dc67c2
Son-Sumin <114986832+Son-Sumin@users.noreply.github.com>
2022-10-09 02:21:07 +0900

    방법2에서 i의 range는 2~루트x+1을 의미함

--- 소수판별.py
+++ 소수판별.py
@@ -14,7 +14,7 @@ print(prime)
 # 방법2
 def isPrime(x) :
     if x == 1: return False
-    for i in range(2,int(x**1.5)) :
+    for i in range(2,int(x**0.5)+1) :
         if x % i == 0 :
             return False
     return True
